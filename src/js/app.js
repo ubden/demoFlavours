ce551d3a34e1362bce2c097c6964b8518f0d2730
jazdosanjh <jas@dosanjh.co.uk>
2019-02-17 18:38:08 +0000

    Added Google map to the reservation section. Also changed one of the images from the gallery

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,16 @@ $('.overlay').on('click', function(){
     $(".button a").toggleClass('btn-open').toggleClass('btn-close');
     open = false;
 });
+
+// Map
+function initMap() {
+    // Your location
+    const loc = { lat: 42.361145, lng: -71.057083 };
+    // Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map'), {
+      zoom: 14,
+      center: loc
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map });
+  }  
