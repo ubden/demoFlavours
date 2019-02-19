6efa98ee5b821c78012ff3cb4b19adb49eb14565
jazdosanjh <jas@dosanjh.co.uk>
2019-02-19 13:59:32 +0000

    Added animate on scroll to each section of the site using vanilla JavaScript

--- src/js/app.js
+++ src/js/app.js
@@ -4,15 +4,41 @@ $('#toggle').click(function() {
   $('#overlay').toggleClass('open');
  });
 
-// Map
+// Google Map
 function initMap() {
-    // Your location
-    const loc = { lat: 42.361145, lng: -71.057083 };
-    // Centered map on location
-    const map = new google.maps.Map(document.querySelector('.map'), {
-      zoom: 14,
-      center: loc
-    });
-    // The marker, positioned at location
-    const marker = new google.maps.Marker({ position: loc, map: map });
-  }  
+  const loc = { lat: 42.361145, lng: -71.057083 };
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Animate on scroll
+const animateHTML = function() {
+  let elems,
+      windowHeight
+  
+  let init = function() {
+    elems = document.getElementsByClassName("hidden");
+    windowHeight = window.innerHeight;
+    _addEventHandlers();
+  }
+  
+  const _addEventHandlers = function() {
+      window.addEventListener("scroll", _checkPosition);
+      window.addEventListener("resize", init)
+  }
+  const _checkPosition = function() {
+    for ( let i = 0; i < elems.length; i++ ) {
+      let posFromTop = elems[i].getBoundingClientRect().top;
+      if ( posFromTop - windowHeight <= 0) { 
+        elems[i].className = elems[i].className.replace( "hidden", "fade-in");
+      }
+    }    
+  }
+  return {
+    init: init
+  }
+}
+animateHTML().init();
